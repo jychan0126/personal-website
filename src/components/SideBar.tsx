387c4d895c92b2a2074e875d5a830d4c7e35b0d6
jychan0126 <s2628355@gmail.com>
2022-02-11 22:01:43 +0800

    Design the logo and the sidebar layout.

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { createTheme, Drawer, ListItemButton, Paper, Typography } from "@mui/material";
+import { Card, CardContent, CardMedia, createTheme, Drawer, Grid, ListItemButton, Paper, Stack, Typography } from "@mui/material";
 import CssBaseline from '@mui/material/CssBaseline';
 import { Toolbar, AppBar } from "@mui/material";
 import { Divider, List, ListItem, ListItemIcon, ListItemText } from "@mui/material";
@@ -9,11 +9,45 @@ import { ThemeProvider } from "@emotion/react";
 import { useAppDispatch, useAppSelector } from "../app/hook";
 import { selectControlPage, store } from "../slice/controlPage";
 import { PAGE } from "../type.d";
+import { Box, color, textAlign } from "@mui/system";
+import logo from '../img/Logo_big.png'
 
 const drawerWidth = 300;
 
 const sidebarTheme = createTheme({
   components: {
+    MuiCard: {
+      styleOverrides: {
+        root: {
+          backgroundColor: '#0000',
+          color: 'white',
+          boxShadow: 'none',
+          paddingTop: '15px',
+          paddingBottom: '15px',
+          paddingLeft: '10px',
+          paddingRight: '10px',
+          justifyContent: 'space-around'
+        }
+      }
+    },
+    MuiCardContent: {
+      styleOverrides: {
+        root: {
+          // paddingLeft: '40px',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center'
+        }
+      }
+    },
+    MuiCardMedia: {
+      styleOverrides: {
+        img: {
+          width: '105px',
+          padding: '16px'
+        }
+      }
+    },
     MuiDrawer: {
       styleOverrides: {
         paper: {
@@ -62,8 +96,27 @@ export default function SideBar() {
       variant="permanent"
       anchor="left"
     >
-      <Paper>Ch-Ying</Paper>
-      {/* <Divider /> */}
+      <Card sx={{ display: 'flex' }}>
+        <CardMedia
+          component="img"
+          image={logo}
+          // sx={{flexGrow: '1'}}
+          alt="Live from space album cover"
+        />
+        <Box sx={{ display: 'flex'}}>
+          <CardContent>
+            <Typography component="span" variant="h5">
+              詹居穎
+            </Typography>
+            <Typography variant="subtitle1" color="white" component="div" sx={{ marginTop: '8px' }}>
+              Chan, Chu-Ying
+            </Typography>
+          </CardContent>
+          <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+          </Box>
+        </Box>
+      </Card>
+
       <List>
         {[PAGE.AboutMe, 'Starred', 'Send email', 'Drafts'].map((text, index) => (
           <ListItemButton onClick={() => changePage(text)} key={text}>
